3a56c1d41334d140d1b40f06bd1bfca0dc6922c2
oscar <oscar@oscar.(none)>
2016-04-27 13:23:18 +0200

    version 1.0 of changed detection

--- process_frame.c
+++ process_frame.c
@@ -11,13 +11,23 @@
 #include "template.h"
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define IMG_SIZE NUM_COLORS*OSC_CAM_MAX_IMAGE_WIDTH*OSC_CAM_MAX_IMAGE_HEIGHT
 
+void ChangeDetection();
+void SetBackground();
+void Erode_3x3(int InIndex, int OutIndex);
+void Dilate_3x3(int InIndex, int OutIndex);
+
 const int nc = OSC_CAM_MAX_IMAGE_WIDTH;
 const int nr = OSC_CAM_MAX_IMAGE_HEIGHT;
+const int Border = 1;
+const int frgLimit = 100;
 
 int TextColor;
+float bgrImg[IMG_SIZE];
+const float avgFac = 0.99;
 
 
 void ResetProcess()
@@ -27,6 +37,8 @@ void ResetProcess()
 		TextColor = MAGENTA;
 	else
 		TextColor = CYAN;
+
+	SetBackground();
 }
 
 
@@ -44,15 +56,104 @@ void ProcessFrame()
 
 		//example for drawing output
 		//draw line
-		DrawLine(10, 100, 200, 20, RED);
+		//DrawLine(10, 100, 200, 20, RED);
 		//draw open rectangle
-		DrawBoundingBox(20, 10, 50, 40, false, GREEN);
+		//DrawBoundingBox(20, 10, 50, 40, false, GREEN);
 		//draw filled rectangle
-		DrawBoundingBox(80, 100, 110, 120, true, BLUE);
-		DrawString(200, 200, strlen(Text), TINY, TextColor, Text);
+		//DrawBoundingBox(80, 100, 110, 120, true, BLUE);
+		//DrawString(200, 200, strlen(Text), TINY, TextColor, Text);
+
+		ChangeDetection();
+		Erode_3x3(THRESHOLD, INDEX0);
+		Dilate_3x3(INDEX0, THRESHOLD);
 	}
 }
 
 
+void ChangeDetection() {
+	int r, c;
+	// set result buffer to zero
+	memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
+
+	//loop over the rows
+	for(r = Border*nc; r<(nr-Border)*nc; r+= nc){
+		// loop over the columns
+		for(c=Border; c<(nc-Border); c++) {
+			float pImg = data.u8TempImage[SENSORIMG][r+c];
+			float pBgr = bgrImg[r+c];
+			float Dif = fabs(pImg-pBgr);
+
+			// if the difference is larger than threshold value
+			if(Dif>data.ipc.state.nThreshold){
+				// set pixel value to 255 in THRESHOLD
+				data.u8TempImage[THRESHOLD][r+c] = 255;
+				// increase foreground counter
+				data.u8TempImage[INDEX1][r+c]++;
+				// check whether limit is reached
+				if(data.u8TempImage[INDEX1][r+c] == frgLimit) {
+					// set pixel to background
+					bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
+					// reset foreground counter
+					data.u8TempImage[INDEX1][r+c] = 0;
+				}
+
+			} else{
+				// update background image
+				bgrImg[r+c] = avgFac*bgrImg[r+c] + (1-avgFac)*(float)data.u8TempImage[SENSORIMG][r+c];
+
+				// set value for display
+				data.u8TempImage[BACKGROUND][r+c] = (unsigned char) bgrImg[r+c];
+
+				// set foreground counter to zero
+				data.u8TempImage[INDEX1][r+c] = 1;
+			}
+		}
+
+
+
+	}
+}
+
+void SetBackground() {
+	int r, c;
+
+	// loop over the rows
+	for(r = Border*nc; r<(nr-Border)*nc; r += nc) {
+		// loop over the columns
+		for( c = Border; c< (nc-Border); c++) {
+			bgrImg[r+c] = (float) data.u8TempImage[SENSORIMG][r+c];
+
+		}
+	}
+	memset(data.u8TempImage[INDEX1], 0, IMG_SIZE);
+}
+
+void Erode_3x3(int InIndex, int OutIndex) {
+	int c, r;
+	for(r = Border*nc; r<(nr-Border)*nc; r+= nc) {
+		for(c=Border; c<(nc-Border); c++){
+			unsigned char* p= &data.u8TempImage[InIndex][r+c];
+
+			data.u8TempImage[OutIndex][r+c] =
+					*(p-nc-1) 	& *(p-nc) 	& *(p-nc+1) &
+					*(p-1)		& *(p)		& *(p+1)	&
+					*(p+nc-1) 	& *(p+nc) 	& *(p+nc+1);
+		}
+	}
+}
+
+void Dilate_3x3(int InIndex, int OutIndex) {
+	int c, r;
+	for(r = Border*nc; r<(nr-Border)*nc; r+= nc) {
+		for(c=Border; c<(nc-Border); c++){
+			unsigned char* p= &data.u8TempImage[InIndex][r+c];
+
+			data.u8TempImage[OutIndex][r+c] =
+					*(p-nc-1) 	| *(p-nc) 	| *(p-nc+1) |
+					*(p-1)		| *(p)		| *(p+1)	|
+					*(p+nc-1) 	| *(p+nc) 	| *(p+nc+1);
+		}
+	}
+}
 
 
